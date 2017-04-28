eb585a0620f9f22fa6fe14fd8aac8e82c1e7889e
kenstin <k3nstin@gmail.com>
2017-04-29 01:40:02 +0200

    Add a unit test project

---
+++ Portierniaktosiedzi.Tests/Placeholder.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Portierniaktosiedzi.Tests
+{
+    public class Placeholder
+    {
+        [Fact]
+        public void AlwaysTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
