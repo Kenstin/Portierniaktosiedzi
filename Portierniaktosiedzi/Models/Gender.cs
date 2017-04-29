b1282c1696f5038d35320d91cf2edf7d55fd8a4f
RePreus <panma@vp.pl>
2017-04-29 18:13:59 +0200

    Add Employee and Gender classes (#1)

---
+++ Portierniaktosiedzi/Models/Gender.cs
@@ -0,0 +1,15 @@
+﻿namespace Portierniaktosiedzi.Models
+{
+        public enum Gender
+        {
+            /// <summary>
+            /// Represents a man
+            /// </summary>
+            Man,
+
+            /// <summary>
+            /// Represents a woman
+            /// </summary>
+            Woman
+        }
+}
