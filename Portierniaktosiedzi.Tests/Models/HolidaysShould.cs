2b2cc3e369eabec03869a86e0dc8b9ce8f105609
kenstin <k3nstin@gmail.com>
2017-04-29 18:43:25 +0200

    Add Holidays.CalculateEasterDate
    Remove placeholder unit tests
    Add necessary nuget packages
    Disable SA1407 and localization warnings

---
+++ Portierniaktosiedzi.Tests/Models/HolidaysShould.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Portierniaktosiedzi.Models;
+using Xunit;
+
+namespace Portierniaktosiedzi.Tests.Models
+{
+    public class HolidaysShould
+    {
+        [Theory]
+        [InlineData(2016, 3, 27)]
+        [InlineData(2017, 4, 16)]
+        [InlineData(2018, 4, 1)]
+        public void CalculateEasterDate(int year, int outMonth, int outDay)
+        {
+            var holidays = new Holidays(year);
+            Assert.True(DateTime.Equals(holidays.CalculateEasterDate(), new DateTime(year, outMonth, outDay)));
+        }
+
+        [Fact]
+        public void ThrowArgOutOfRangeExceptionGivenYearBelow1()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Holidays(0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Holidays(int.MinValue));
+        }
+    }
+}
