8c8cfc13793fe72e524399e4d281179b7deae167
Unknown <panma@vp.pl>
2017-04-29 17:31:48 +0200

    Add Employee and Gender classes

---
+++ Portierniaktosiedzi/Models/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portierniaktosiedzi.Models
+{
+    public class Employee
+    {
+        public Employee(int posts, Gender gender, string name)
+        {
+            Posts = posts;
+            Gender = gender;
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public Gender Gender { get; }
+
+        public int Posts { get; }
+
+        public int PostsToWorkingHours(int month, int year)
+        {
+            int hours, workingdays;
+            int daysinmonth = System.DateTime.DaysInMonth(year, month);
+            workingdays = System.DateTime.DaysInMonth(year, month);
+            for (int i = 1; i <= daysinmonth; i++)
+            {
+                DateTime dateValue = new DateTime(year, month, i);
+                if (dateValue.ToString("dddd", CultureInfo.InvariantCulture) == "Saturday" || dateValue.ToString("dddd", CultureInfo.InvariantCulture) == "Sunday")
+                {
+                    workingdays--;
+                }
+            }
+
+            hours = (Posts * (workingdays * 8)) - ((Posts * (workingdays * 8)) % 8);
+            return hours;
+        }
+    }
+}
---
+++ Portierniaktosiedzi/Models/Gender.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portierniaktosiedzi.Models
+{
+        public enum Gender
+        {
+            /// <summary>
+            /// Represents a man
+            /// </summary>
+            Man,
+
+            /// <summary>
+            /// Represents a woman
+            /// </summary>
+            Woman
+        }
+}
