93d33d210a89cfff6a47abd2df567a48af4da07d
Kenstin <k3nstin@gmail.com>
2017-04-30 13:27:53 +0200

    Add IHolidays, Holidays (#3)
    * Add Holidays.CalculateEasterDate
    
    Remove placeholder unit tests
    Add necessary nuget packages
    Disable SA1407 and localization warnings
    
    * Move Models/Holidays.cs to Utility/Holidays.cs
    
    * Add IHolidays
    
    * Fix namespaces

---
+++ Portierniaktosiedzi/Utility/Holidays.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Portierniaktosiedzi.Utility
+{
+    public class Holidays : IHolidays
+    {
+        private readonly int year;
+
+        public Holidays(int year)
+        {
+            if (year < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(year), "Value must be greater than 0");
+            }
+
+            this.year = year;
+        }
+
+        public Collection<DateTime> GetHolidays() => new Collection<DateTime>
+            {
+                CalculateEasterDate()
+            };
+
+        public DateTime CalculateEasterDate()
+        {
+            int month = 3;
+
+            int g = year % 19;
+            int c = year / 100;
+            int h = (c - c / 4 - (8 * c + 13) / 25 + 19 * g + 15) % 30;
+            int i = h - h / 28 * (1 - h / 28 * (29 / (h + 1)) * ((21 - g) / 11));
+
+            int day = i - (year + year / 4 + i + 2 - c + c / 4) % 7 + 28;
+
+            if (day > 31)
+            {
+                month++;
+                day -= 31;
+            }
+
+            return new DateTime(year, month, day);
+        }
+    }
+}
