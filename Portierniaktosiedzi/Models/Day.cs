77c0e482e3c0f7a32566396ad713ab8bdf0e9404
kenstin <k3nstin@gmail.com>
2017-05-01 23:49:26 +0200

    Add timetable
    XD REFACTOR THIS PLZ

---
+++ Portierniaktosiedzi/Models/Day.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace Portierniaktosiedzi.Models
+{
+    public class Day
+    {
+        public Employee[] Shifts { get; } = new Employee[3];
+
+        /// <summary>
+        /// Checks whether all shifts are full
+        /// </summary>
+        /// <returns>True if all shifts are full, false otherwise</returns>
+        public bool IsFull() => Shifts.All(employee => employee != null);
+    }
+}
