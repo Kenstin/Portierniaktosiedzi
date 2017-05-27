e8ea58d430f10f2196c53bc3dff8e6544f5667ed
kenstin <k3nstin@gmail.com>
2017-05-27 23:51:55 +0200

    Extract some methods to reduce cyclomatic complexity

--- Portierniaktosiedzi/Models/TimetableGenerator.cs
+++ Portierniaktosiedzi/Models/TimetableGenerator.cs
@@ -65,13 +65,12 @@ namespace Portierniaktosiedzi.Models
             foreach (var weekend in weekends)
             {
                 weekendsOff.Add(nextEmployee, weekend);
-                // employee[e].week = weekends[i] // <= to employeeowi[e] przypisujemy weekend[i]
                 if (BruteForce())
                 {
                     return true;
-                }// i wywolujac rekurencje robimy to samo dla kolejnych
+                } // i wywolujac rekurencje robimy to samo dla kolejnych
 
-                weekendsOff.Remove(nextEmployee); //jesli XD() zwrocilo nam false to znaczy, ze nie udalo sie wygenerowac poprawnego grafiku i cofamy przypisanie weekendu
+                weekendsOff.Remove(nextEmployee); //jesli BruteForce() zwrocilo nam false to znaczy, ze nie udalo sie wygenerowac poprawnego grafiku i cofamy przypisanie weekendu
             }
 
             return false; //zwracamy false bo kombinacja nie byla prawidlowa
@@ -79,11 +78,6 @@ namespace Portierniaktosiedzi.Models
 
         private bool TryMake()
         {
-            /*for (var i = 0; i < employees.Count; i++)
-            {
-                weekendsOff[employees[i]] = weekends[i % weekends.Count];
-            }*/
-
             for (var i = 1; i <= timetable.Days.RightArrayLength; i++)
             {
                 timetable.Days[i] = new Day();
@@ -92,23 +86,74 @@ namespace Portierniaktosiedzi.Models
             workingHoursLeft = employees.ToDictionary(
                 employee => employee, employee => employee.GetWorkingHours(timetable.Month.Month, timetable.Month.Year, holidays));
 
-            var boys = employees.Where(e => e.Gender == Gender.Man).ToList();
+            FillThirdShift();
+            FillDaysOff();
+            FillSecondShift();
+            FillFirstShift();
+            FillSchoolEmployees();
 
-            for (var i = 1; i <= timetable.Days.RightArrayLength; i++)
+            if (timetable.Days.Any(day => day.Shifts.Any(shift => shift == null)))
             {
-                var first = boys.FirstOrDefault(e => MeetsCriteria(e, i, 2));
-                AddToShift(first, i, 2);
+                return false;
             }
 
-            /*foreach (var day in days)
+            if (workingHoursLeft.Any(pair => pair.Value != 0))
             {
-                var first = girls.First(e => workingHoursLeft[e] > 0 && MeetsCriteria());
-                workingHoursLeft[first] -= 8;
-                day.Shifts[1] = first;
-            }*/
+                var i = workingHoursLeft.Sum(pair => pair.Value);
+                if (i < bestCombination.Item2)
+                {
+                    bestCombination = Tuple.Create(new Dictionary<Employee, Tuple<DateTime, DateTime>>(weekendsOff), i);
+                }
+            }
+            else
+            {
+                return true;
+            }
 
-           // var employeesLeft = employees.Where(e => workingHoursLeft[e] > 0).ToList();
+            return false;
+        }
 
+        private void FillSchoolEmployees()
+        {
+            var days = timetable.Days.Where(day => !day.IsFull());
+            foreach (var day in days)
+            {
+                if (day.Shifts[0] == null)
+                {
+                    day.Shifts[0] = schoolEmployee;
+                }
+            }
+        }
+
+        private void FillFirstShift()
+        {
+            for (var i = 1; i <= timetable.Days.RightArrayLength; i++)
+            {
+                if (timetable.Days[i].Shifts[0] == null)
+                {
+                    var emp = employees.FirstOrDefault(e => MeetsCriteria(e, i, 0));
+                    if (emp != null)
+                    {
+                        AddToShift(emp, i, 0);
+                    }
+                }
+            }
+        }
+
+        private void FillSecondShift()
+        {
+            for (var i = 1; i <= timetable.Days.RightArrayLength; i++)
+            {
+                if (timetable.Days[i].Shifts[1] == null)
+                {
+                    var emp = employees.FirstOrDefault(e => MeetsCriteria(e, i, 1));
+                    AddToShift(emp, i, 1);
+                }
+            }
+        }
+
+        private void FillDaysOff()
+        {
             var daysOff = new List<DateTime>();
             daysOff.AddRange(holidays.HolidayDates.Where(date => date.Month == timetable.Month.Month));
             daysOff.AddRange(timetable.Month.GetSaturdaysAndSundays());
@@ -122,71 +167,17 @@ namespace Portierniaktosiedzi.Models
                 emp = employees.FirstOrDefault(e => MeetsCriteria(e, date.Day, 0));
                 AddToShift(emp, date.Day, 0);
             }
+        }
 
-            /*for (var i = 0; i < days.Length; i++)
-            {
-                var day = days[i];
-                for (var j = 0; j < day.Shifts.Length; j++)
-                {
-                    if (day.Shifts[j] == null)
-                    {
-                        var emp = employeesLeft.First(e => workingHoursLeft[e] > 0 &&
-                                                           MeetsCriteria(e, workingHoursLeft, i, j));
-                        day.Shifts[j] = emp;
-                        workingHoursLeft[emp] -= 8;
-                    }
-                }
-            }*/
+        private void FillThirdShift()
+        {
+            var boys = employees.Where(e => e.Gender == Gender.Man).ToList();
 
             for (var i = 1; i <= timetable.Days.RightArrayLength; i++)
             {
-                if (timetable.Days[i].Shifts[1] == null)
-                {
-                    var emp = employees.FirstOrDefault(e => MeetsCriteria(e, i, 1));
-                    AddToShift(emp, i, 1);
-                }
+                var first = boys.FirstOrDefault(e => MeetsCriteria(e, i, 2));
+                AddToShift(first, i, 2);
             }
-
-            for (var i = 1; i <= timetable.Days.RightArrayLength; i++)
-            {
-                if (timetable.Days[i].Shifts[0] == null)
-                {
-                    var emp = employees.FirstOrDefault(e => MeetsCriteria(e, i, 0));
-                    if (emp != null)
-                    {
-                        AddToShift(emp, i, 0);
-                    }
-                }
-            }
-
-            var days = timetable.Days.Where(day => !day.IsFull());
-            foreach (var day in days)
-            {
-                if (day.Shifts[0] == null)
-                {
-                    day.Shifts[0] = schoolEmployee;
-                }
-            }
-
-            if (timetable.Days.Any(day => day.Shifts.Any(shift => shift == null)))
-            {
-                return false;
-            }
-
-            if (workingHoursLeft.All(pair => pair.Value == 0))
-            {
-                return true;
-            }
-            else
-            {
-                var i = workingHoursLeft.Sum(pair => pair.Value);
-                if (i < bestCombination.Item2)
-                {
-                    bestCombination = Tuple.Create(new Dictionary<Employee, Tuple<DateTime, DateTime>>(weekendsOff), i);
-                }
-            }
-
-            return false;
         }
 
         [SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1503:BracesMustNotBeOmitted", Justification = "Reviewed.")]
@@ -200,129 +191,22 @@ namespace Portierniaktosiedzi.Models
             if (day == weekendsOff[employee].Item1.Day || day == weekendsOff[employee].Item2.Day)
                 return false;
 
-            switch (shift) //checks two-shift break
-            {
-                case 2:
-                    if (timetable.Days[day].Shifts[1] == employee || timetable.Days[day].Shifts[0] == employee || timetable.Days.ElementAtOrDefault(day + 1)?.Shifts[0] == employee || timetable.Days.ElementAtOrDefault(day + 1)?.Shifts[1] == employee)
-                        return false;
-                    break;
-                case 1:
-                    if (timetable.Days[day].Shifts[0] == employee || timetable.Days[day - 1].Shifts[2] == employee || timetable.Days[day].Shifts[2] == employee || timetable.Days.ElementAtOrDefault(day + 1)?.Shifts[0] == employee)
-                        return false;
-                    break;
-                case 0:
-                    if (timetable.Days[day - 1].Shifts[2] == employee || timetable.Days[day - 1].Shifts[1] == employee || timetable.Days[day].Shifts[1] == employee || timetable.Days[day].Shifts[2] == employee)
-                        return false;
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(shift));
-            }
+            if (!CheckTwoShiftBreak(employee, day, shift))
+                return false;
 
-           /* if (day % 7 == 6) //&& shift == 1) //35h
-            {
-                int biggestGapSoFar = 0;
-                int biggestGapNow = 0;
-                for (int i = day - 5; i < day; i++)
-                {
-                    foreach (var j in timetable.Days[i].Shifts)
-                    {
-                        if (j != employee)
-                        {
-                            biggestGapNow++;
-                            if (biggestGapNow > biggestGapSoFar)
-                            {
-                                biggestGapSoFar = biggestGapNow;
-                            }
-                        }
-                        else
-                        {
-                            biggestGapNow = 0;
-                        }
-                    }
-                }
+            if (!Check5DaysFromMonToSun(employee, date))
+                return false;
 
-                if (biggestGapNow > 0)
-                {
-                    biggestGapNow += shift;
-                    if (biggestGapNow > biggestGapSoFar)
-                    {
-                        biggestGapSoFar = biggestGapNow;
-                    }
-                }
+            if (!Check5DaysInARow(employee, day, date))
+                return false;
 
-                if (biggestGapSoFar < 5)
-                    return false;
-            }
-            */
-            {
-                //5 days per week
-                int daysOff = 0;
-                DateTime tmp = date.Clone();
-                while (tmp.DayOfWeek != DayOfWeek.Monday)
-                {
-                    tmp = tmp.AddDays(-1);
-                    if (tmp.Month == date.Month && !timetable.Days[tmp.Day].IsThere(employee))
-                    {
-                        daysOff++;
-                    }
-                    else if (tmp.Month == date.Month - 1 &&
-                             timetable.Days[tmp.Day - DateTime.DaysInMonth(tmp.Year, tmp.Month)].IsThere(employee))
-                    {
-                        daysOff++;
-                    }
-                }
-
-                tmp = date.Clone();
-                while (tmp.DayOfWeek != DayOfWeek.Sunday)
-                {
-                    tmp = tmp.AddDays(1);
-                    if (tmp.Month == date.Month && !timetable.Days[tmp.Day].IsThere(employee))
-                    {
-                        daysOff++;
-                    }
-                    else if (tmp.Month == date.Month - 1 &&
-                             timetable.Days[tmp.Day - DateTime.DaysInMonth(tmp.Year, tmp.Month)].IsThere(employee))
-                    {
-                        daysOff++;
-                    }
-                    else if (tmp.Month == date.Month + 1)
-                    {
-                        daysOff++;
-                    }
-                }
-
-                if (daysOff < 2)
-                    return false;
+            return true;
         }
 
-            /*if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) //5 days per week (mon-sun)
-            {
-                int daysOff = 0;
-
-                DateTime tmp = date.AddDays(-1);
-                do
-                {
-                    if (tmp.Month == date.Month && !timetable.Days[tmp.Day].IsThere(employee))
-                    {
-                        daysOff++;
-                    }
-                    else if (tmp.Month == date.Month - 1 && timetable.Days[tmp.Day - DateTime.DaysInMonth(tmp.Year, tmp.Month)].IsThere(employee))
-                    {
-                        daysOff++;
-                    }
-
-                    tmp = tmp.AddDays(-1);
-                }
-                while (tmp.DayOfWeek != DayOfWeek.Sunday); //na pewno xD?
-
-                if (daysOff < 2)
-                {
-                    return false;
-                }
-            }*/
-
-            int workingDays = 0;
-            for (var i = day - 5; i < day; i++) //5 days in a row, to tez jest zjebane
+        private bool Check5DaysInARow(Employee employee, int day, DateTime date)
+        {
+            var workingDays = 0;
+            for (var i = day - 5; i < day; i++)
             {
                 if (timetable.Days[i].IsThere(employee))
                 {
@@ -330,16 +214,83 @@ namespace Portierniaktosiedzi.Models
                 }
             }
 
+            var tmp = date.Clone().AddDays(1);
+            while (timetable.Days[tmp.Day].IsThere(employee))
             {
-                DateTime tmp = date.Clone().AddDays(1);
-                while (timetable.Days[tmp.Day].IsThere(employee))
-                {
-                    workingDays++;
-                    tmp = tmp.AddDays(1);
-                }
+                workingDays++;
+                tmp = tmp.AddDays(1);
             }
 
             return workingDays < 5;
+        }
+
+        private bool Check5DaysFromMonToSun(Employee employee, DateTime date)
+        {
+            var daysOff = 0;
+            var tmp = date.Clone();
+            while (tmp.DayOfWeek != DayOfWeek.Monday)
+            {
+                tmp = tmp.AddDays(-1);
+                if (tmp.Month == date.Month && !timetable.Days[tmp.Day].IsThere(employee))
+                {
+                    daysOff++;
+                }
+                else if (tmp.Month == date.Month - 1 &&
+                         timetable.Days[tmp.Day - DateTime.DaysInMonth(tmp.Year, tmp.Month)].IsThere(employee))
+                {
+                    daysOff++;
+                }
+            }
+
+            tmp = date.Clone();
+            while (tmp.DayOfWeek != DayOfWeek.Sunday)
+            {
+                tmp = tmp.AddDays(1);
+                if (tmp.Month == date.Month && !timetable.Days[tmp.Day].IsThere(employee))
+                {
+                    daysOff++;
+                }
+                else if (tmp.Month == date.Month - 1 &&
+                         timetable.Days[tmp.Day - DateTime.DaysInMonth(tmp.Year, tmp.Month)].IsThere(employee))
+                {
+                    daysOff++;
+                }
+                else if (tmp.Month == date.Month + 1)
+                {
+                    daysOff++;
+                }
+            }
+
+            return daysOff >= 2;
+        }
+
+        [SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1503:BracesMustNotBeOmitted", Justification = "Reviewed.")]
+        private bool CheckTwoShiftBreak(Employee employee, int day, int shift)
+        {
+            switch (shift)
+            {
+                case 2:
+                    if (timetable.Days[day].Shifts[1] == employee || timetable.Days[day].Shifts[0] == employee ||
+                        timetable.Days.ElementAtOrDefault(day + 1)?.Shifts[0] == employee ||
+                        timetable.Days.ElementAtOrDefault(day + 1)?.Shifts[1] == employee)
+                        return false;
+                    break;
+                case 1:
+                    if (timetable.Days[day].Shifts[0] == employee || timetable.Days[day - 1].Shifts[2] == employee ||
+                        timetable.Days[day].Shifts[2] == employee ||
+                        timetable.Days.ElementAtOrDefault(day + 1)?.Shifts[0] == employee)
+                        return false;
+                    break;
+                case 0:
+                    if (timetable.Days[day - 1].Shifts[2] == employee || timetable.Days[day - 1].Shifts[1] == employee ||
+                        timetable.Days[day].Shifts[1] == employee || timetable.Days[day].Shifts[2] == employee)
+                        return false;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(shift));
+            }
+
+            return true;
         }
     }
 }
