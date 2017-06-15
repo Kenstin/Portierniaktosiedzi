08d0c627418b241555da618af94c15dd980a4310
kenstin <k3nstin@gmail.com>
2017-06-15 18:51:09 +0200

    Revert "Merge branch 'master' into feature-gui"
    This reverts commit e488ca8e9de0cad0718313940ccf4548371d630d.

--- Portierniaktosiedzi/Models/Timetable.cs
+++ Portierniaktosiedzi/Models/Timetable.cs
@@ -22,7 +22,9 @@ namespace Portierniaktosiedzi.Models
             {
                 throw new ArgumentNullException(nameof(month));
             }
-            
+
+            var days = daysBefore.ToList();
+
             Month = month.Clone();
 
             try
