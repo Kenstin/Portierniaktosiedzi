2ae8d68fc5a01666b7e47bb4d4be7e0698cde840
Kenstin <k3nstin@gmail.com>
2017-06-20 00:31:28 +0200

    Translate Employee datagrid columns (#36)
    + convert gender to Polish
    Fixes #32

---
+++ Portierniaktosiedzi/Converters/GenderToPolishConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Data;
+using Portierniaktosiedzi.Models;
+
+namespace Portierniaktosiedzi.Converters
+{
+    public class GenderToPolishConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return value.Equals(Gender.Man) ? "Mezczyzna" : "Kobieta";
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException("Cannot convert back");
+        }
+    }
+}
