82ba6ee89ec2238a89af7800b19c1a5bfcba44f2
Unknown <k3nstin@gmail.com>
2017-09-13 23:42:21 +0200

    Add TimetableContext.cs

---
+++ Portierniaktosiedzi/Database/TimetableContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Portierniaktosiedzi.Models;
+
+namespace Portierniaktosiedzi.Database
+{
+    public class TimetableContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var source = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "Portierniaktosiedzi/Portiernia.db");
+            optionsBuilder.UseSqlite("Data Source=" + source);
+        }
+    }
+}
